aa6c7c28d9dea6d3b255d71b98c673b47f58d30c
Sonya McCree <smccree@bu.edu>
2019-09-29 16:26:20 -0400

    Initial Project Upload
    Initial project upload - game progress as of 29 Sept 2019

---
+++ Home Alone V2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        // Movement - constant movement speed
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Home Alone V2/Assets/Scripts/SimulationController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Class controls the pet's health bar
+public class SimulationController : MonoBehaviour
+{
+    [SerializeField]
+    private int s; //how happy is our pet? bar goes 0 - 100 where 0 is very stressed and 100 is very happy
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateStatus();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateStatus()
+    {
+        //set value for happiness meter
+        if(!PlayerPrefs.HasKey("happiness"))
+        {
+            happiness = 100;
+            PlayerPrefs.SetInt("happiness", happiness);
+        }
+        else
+        {
+            happiness = PlayerPrefs.GetInt("happiness");
+        }
+
+         
+    }
+
+    public int happiness
+    {
+        get { return happiness; }
+        set { happiness = value; }
+    }
+}
