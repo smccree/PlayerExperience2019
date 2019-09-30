fb23d2afb2ed2465105fe3d4ee77f4f2d232502b
Sonya McCree <smccree@bu.edu>
2019-09-29 20:48:49 -0400

    object interactions scripting update
    implemented basic object interaction system, started work on hunger/happiness bars

---
+++ Home Alone V2/Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    public bool talks; //temporary to set up logic (if true object can talk to player)
+    public string message; //what object says to player
+
+    public void DoInteraction()
+    {
+        //Pick up and remove from screen
+        gameObject.SetActive(false); //turn invisible but not destroyed
+    }
+
+    public void Talk()
+    {
+        Debug.Log(message); //temporary - add UI later.
+    }
+
+}
---
+++ Home Alone V2/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject currentInterobj = null; //object we are currently interacting with
+    public InteractionObject currentInterobjScript = null;
+
+    void Update()
+    {
+        if(Input.GetButtonDown ("Interact") && currentInterobj)
+        {
+            //Do something with the object
+            //send message to object interacting with
+            currentInterobj.SendMessage("DoInteraction");
+
+            //check to see if the object has a message and talks
+            if (currentInterobjScript.talks)
+            {
+                currentInterobjScript.Talk();
+            }
+        }
+
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("inter_object"))
+        {
+            Debug.Log(collision.name);
+            currentInterobj = collision.gameObject; //player is in range of interactable object
+            currentInterobjScript = currentInterobj.GetComponent<InteractionObject>();
+
+            //testing
+            HealthBarScript.health -= 10f;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("inter_object"))
+        {
+            if(collision.gameObject == currentInterobj)
+            {
+                currentInterobj = null; //player has walked out of range of interactable object
+            } 
+            
+        }
+    }
+}
